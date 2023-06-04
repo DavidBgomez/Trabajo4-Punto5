6567cabc1314d829a9f1d249a9449911b703f767
DavidBgomez <94724509+DavidBgomez@users.noreply.github.com>
2023-06-03 23:20:19 -0500

    Add files via upload

---
+++ Main.java
@@ -0,0 +1,10 @@
+package Personas;
+
+public class Main {
+    public static void main(String[] args) {
+        VentanaPrincipal miVentanaPrincipal; 
+        miVentanaPrincipal= new VentanaPrincipal();
+        miVentanaPrincipal.setVisible(true); 
+    }
+    
+}
---
+++ Persona.java
@@ -0,0 +1,15 @@
+package Personas;
+
+public class Persona {
+    String nombre;
+    String apellido;
+    String telefono;
+    String direccion;
+
+    public Persona(String nombre, String apellido, String telefono, String direccion) {
+        this.nombre = nombre;
+        this.apellido = apellido;
+        this.telefono = telefono;
+        this.direccion = direccion;
+    }   
+}
